b844b8f15d2e62fc302140ffb0d6f4cc6f63f465
verraoktari <71430977+verraoktari@users.noreply.github.com>
2020-11-10 14:35:23 -0800

    tugas rekweb ci4login
    tugas  rekweb ci4login

---
+++ ci4login/app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea magni rem, optio suscipit vitae repellat eum maxime eveniet ratione in placeat provident, nisi aperiam. Ab excepturi voluptate vero illum quis?</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
